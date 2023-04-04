bb70e2497e8ddc399dedf93ec17e322b4e4526b3
Eduardo Botelho <edu.botelho@yahoo.com>
2023-04-04 17:20:48 -0300

    Terminando a classe LoadDb

--- src/main/java/ApiRest/ApiRestRDSApplication.java
+++ src/main/java/ApiRest/ApiRestRDSApplication.java
@@ -2,7 +2,9 @@ package ApiRest;
 
 import br.com.exemploRDS.apiRest.ApiRestApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApiRestRDSApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiRestApplication.class, args);
--- src/main/java/ApiRest/LoadDB.java
+++ src/main/java/ApiRest/LoadDB.java
@@ -2,17 +2,23 @@ package ApiRest;
 
 import br.com.exemploRDS.apiRest.Person;
 import br.com.exemploRDS.apiRest.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class LoadDB {
 
+    private static final Logger log = LoggerFactory.getLogger(LoadDB.class);
     @Bean
-    public CommandLineRunner commandLineRunner(PersonRepository repository){
+    public CommandLineRunner commandLineRunner(EmployeeRepository employeerepository){
         return args -> {
-            repository.save(new Person(1L,"Patricia","Maldonado"));
-            repository.save(new Person(2L,"Natanael", "Brito"));
-
+            log.info("Log of event save user 1: " +employeerepository.save(new Employee("Patricia",
+                    "Funcionario","Avenida Atlantica,3456")));
+           log.info("Log of event save user 2: "+employeerepository.save(new Employee("John",
+                   "","Rua Faria Lima, 900")));
         };
     }
 }
