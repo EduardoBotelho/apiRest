23f594f3f7e1f1f69d38a78f1fcd89aed70797a9
Eduardo Botelho <edu.botelho@yahoo.com>
2023-04-04 09:34:15 -0300

    Initial commit

---
+++ src/main/java/br/com/exemploRDS/apiRest/Person.java
@@ -0,0 +1,17 @@
+package br.com.exemploRDS.apiRest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    
+    private String nome;
+}
